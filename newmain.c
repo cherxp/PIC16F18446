7a1414b31bd14e6f63cf549c815b30298086fb49
Rajesh Seenivasan <rajesh.seenivasan@gmail.com>
2020-09-23 00:14:37 +0530

    initial version

---
+++ newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: xpress1
+ *
+ * Created on 23 September, 2020, 12:11 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
